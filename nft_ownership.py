b17167c4971dee104979a6c7539dfe86bdc03b67
buvanenko <gbuvanenko@gmail.com>
2024-05-16 13:37:43 +0300

    new ownership check

--- nft_ownership.py
+++ nft_ownership.py
@@ -4,82 +4,71 @@ import config
 import json
 import traceback
 import logging
+import aiofiles
 
-
-
-class SideCollection:
+class Collection:
     def __init__(self, collection):
-        # name и adress -- str
         self.name = str(collection['name'])
         self.address = str(collection['address'])
         self.chat_id = int(collection['chat_id'])
-        
-def load() -> list[SideCollection]:
+
+async def load() -> list[Collection]:
     side_collections = []
-    side_collections_raw = list(json.load(open('side_collections.json')))
+    async with aiofiles.open('_collections.json', mode='r') as f:
+        side_collections_raw = list(json.load(await f.read()))
     try:
-        import requests
-        url = 'https://raw.githubusercontent.com/Soviet-Girls/tonconnect-bot/main/side_collections.json'
-        side_collections_raw_online = list(json.loads(requests.get(url).text))
+        url = 'https://raw.githubusercontent.com/Soviet-Girls/tonconnect-bot/main/_collections.json'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                side_collections_raw_online = list(json.loads(await response.text()))
         if len(side_collections_raw_online) > len(side_collections_raw):
             side_collections_raw = side_collections_raw_online
-    except:
-        pass
+    except Exception as e:
+        logging.exception(e)
     for collection in side_collections_raw:
-        collection = SideCollection(collection)
+        collection = Collection(collection)
         side_collections.append(collection)
     return side_collections
 
-side_collections = []
+_collections = []
 try:
-    side_collections = load()
-    print(f"{len(side_collections)} side collections loaded")
-except:
-    traceback.print_exc()
-    logging.error('ERROR LOADING SIDE COLLECTIONS!')
+    side_collections = asyncio.run(load())
+    print(f"{len(_collections)}  collections loaded")
+except Exception as e:
+    logging.exception(e)
 
-
-async def check(address: str) -> list[SideCollection]:
-    url = f'https://tonapi.io/v2/accounts/{address}/nfts'
+async def check(address: str) -> list[Collection]:
     collections = []
-    _t = 0
-    while True:
-        if _t == 5:
-            break
-        params = {
-            'collection': config.COLLECTION,
-            'limit': 1,
-            'offset': 0,
-            'indirect_ownership': 1
-        }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url=url, params=params) as response:
-                response = await response.json()
-                print(response)
-                try:
-                    if response['error'] == 'rate limit: free tier':
+
+    # добавляем основную коллекцию и ставим её на первое место
+    side_collections.append(
+        Collection({
+            'name': config.COLLECTION_NAME,
+            'address': config.COLLECTION,
+            'chat_id': config.GROUP_CHAT_ID,
+        })
+    )
+    available_collections = [side_collections[-1]] + side_collections[:-1]
+
+    async with aiohttp.ClientSession() as session:
+        for collection in available_collections:
+            url = f'https://tonapi.io/v2/accounts/{address}/nfts'
+            params = {
+                'collection': collection.address,
+                'limit': 1,
+                'offset': 0,
+                'indirect_ownership': 1
+            }
+            _t = 0
+            while _t < 5:
+                async with session.get(url=url, params=params) as response:
+                    response = await response.json()
+                    if response.get('error') == 'rate limit: free tier':
                         await asyncio.sleep(3)
                         _t += 1
                         continue
-                except KeyError:
-                    pass
-                if len(response['nft_items']) > 0:
-                    collections.append(SideCollection({'name': config.COLLECTION_NAME, 'address': config.COLLECTION, 'chat_id': config.GROUP_CHAT_ID}))
-                    break
-    
-    for side_collection in side_collections:
-        url = f'https://tonapi.io/v2/accounts/{address}/nfts'
-        params = {
-            'collection': side_collection.address,
-            'limit': 1,
-            'offset': 0,
-            'indirect_ownership': 1
-        }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url=url, params=params) as response:
-                response = await response.json()
-                if len(response['nft_items']) > 0:
-                    collections.append(side_collection)
-
+                    if len(response['nft_items']) > 0:
+                        collections.append(collection)
+                        break
     logging.info(str(collections))
     return collections
